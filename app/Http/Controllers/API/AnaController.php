0c3de4bfb93a03ba11bb02d54480a05879fe51b5
Ivan Rojo <ivanrojo07@gmail.com>
2018-12-27 13:25:46 -0600

    ids y claves en todas las interfaces, trabajando con la cotización

--- app/Http/Controllers/API/AnaController.php
+++ app/Http/Controllers/API/AnaController.php
@@ -33,6 +33,16 @@ class AnaController extends Controller
         $this->urlPHP ="https://server.anaseguros.com.mx/ananetws/servicetext.asmx?wsdl";
     }
 
+    protected function convertXMLtoArray($xmlelements)
+    {
+        $array = [];
+        foreach ($xmlelements as $value) {
+            $object = json_decode(json_encode($value),true);
+            array_push($array,['id'=>$object['@attributes']['id'],'descripcion'=>$object[0]]);
+        }
+        return $array;
+    }
+
     public function prueba()
     {
         try{
@@ -49,12 +59,7 @@ class AnaController extends Controller
         try{
             $client = new SoapClient($this->url,$this->params);
             $bancosXML = $client->Bancos(["Negocio"=>"1195","Usuario"=>"14275","Clave"=>"kdEDyC9F"]);
-            $bancos=[];
-            foreach (simplexml_load_string($bancosXML->BancosResult)->bancos as $key=>$value) {
-                $object = json_decode(json_encode($value),true);
-                array_push($bancos, ['id'=>$object['@attributes']['id'],'descripcion'=>$object[0]]);
-                // dd($object[0]);
-            }
+            $bancos= $this->convertXMLtoArray(simplexml_load_string($bancosXML->BancosResult)->bancos);
             if (isset($bancos)) {
                 // $bancos = $bancosResp['bancos'];
                 return response()->json(['bancos'=>$bancos],201);
@@ -72,15 +77,14 @@ class AnaController extends Controller
         try{
             $client = new SoapClient($this->url,$this->params);
             $categoriasXML = $client->Categoria(["Negocio"=>"1195","Usuario"=>"14275","Clave"=>"kdEDyC9F"]);
-            $categoriasResp = json_decode(json_encode(simplexml_load_string($categoriasXML->CategoriaResult)),true);
-            dd($categoriasResp);
-            if (isset($categoriasResp['categorias'])) {
-                $categorias = $categoriasResp['categorias'];
-                return response()->json(['categorias'=>$categorias],201);
-            }
-            else{
-                return response()->json(['error'=>"Categorias no encontradas",404]);
-            }
+            dd(simplexml_load_string($categoriasXML->CategoriaResult));
+            // if (isset($categoriasResp['categorias'])) {
+            //     $categorias = $categoriasResp['categorias'];
+            //     return response()->json(['categorias'=>$categorias],201);
+            // }
+            // else{
+            //     return response()->json(['error'=>"Categorias no encontradas",404]);
+            // }
         }catch(SoapFault $fault){
             dd($fault);
         }
@@ -91,13 +95,7 @@ class AnaController extends Controller
             $client = new SoapClient($this->url,$this->params);
             $codigoPostalXML = $client->CodigoPostal(["Estado"=>$estado,"Ciudad"=>$ciudad,"Negocio"=>"1195","Usuario"=>"14275","Clave"=>"kdEDyC9F"]);
             // dd(simplexml_load_string($codigoPostalXML->CodigoPostalResult)->cp);
-            $codigoPostal=[];
-            foreach (simplexml_load_string($codigoPostalXML->CodigoPostalResult)->cp as $value) {
-                $object = json_decode(json_encode($value),true);
-                array_push($codigoPostal, ['id'=>$object['@attributes']['id'],'descripcion'=>$object[0]]);
-
-            }
-            // dd($codigoPostal);
+            $codigoPostal= $this->convertXMLtoArray(simplexml_load_string($codigoPostalXML->CodigoPostalResult)->cp);
             if (isset($codigoPostal)) {
                 return response()->json(['Codigo Postal'=>$codigoPostal],201);
             }
@@ -114,12 +112,7 @@ class AnaController extends Controller
             $client = new SoapClient($this->url,$this->params);
             $coloniaXML = $client->Colonia(["Estado"=>$estado,"Ciudad"=>$ciudad,"CP"=>$cp,"Negocio"=>"1195","Usuario"=>"14275","Clave"=>"kdEDyC9F"]);
             // dd(simplexml_load_string($coloniaXML->ColoniaResult)->colonia);
-            $colonias=[];
-            foreach(simplexml_load_string($coloniaXML->ColoniaResult)->colonia as $value){
-                $object=json_decode(json_encode($value),true);
-                array_push($colonias,['id'=>$object['@attributes']['id'],'descripcion'=>$object[0]]);
-            }
-            // dd($colonias);
+            $colonias=$this->convertXMLtoArray(simplexml_load_string($coloniaXML->ColoniaResult)->colonia);
             if (isset($colonias)) {
                 return response()->json(['colonias'=>$colonias],201);
             }
@@ -135,11 +128,9 @@ class AnaController extends Controller
         try{
             $client = new SoapClient($this->url,$this->params);
             $colorXML = $client->Color(["Negocio"=>"1195","Usuario"=>"14275","Clave"=>"kdEDyC9F"]);
-            // dd($colorXML);
-            $colorResp = json_decode(json_encode(simplexml_load_string($colorXML->ColorResult)),true);
-            // dd($colorResp);
-            if (isset($colorResp['color'])) {
-                $colores = $colorResp['color'];
+            // dd(simplexml_load_string($colorXML->ColorResult)->color);
+            $colores=$this->convertXMLtoArray(simplexml_load_string($colorXML->ColorResult)->color);
+            if (isset($colores)) {
                 return response()->json(['colores'=>$colores],201);
             }
             else{
@@ -155,11 +146,7 @@ class AnaController extends Controller
             $client = new SoapClient($this->url,$this->params);
             $municipioXML = $client->DelMun(["Estado"=>$estado,"Negocio"=>"1195","Usuario"=>"14275","Clave"=>"kdEDyC9F"]);
             // dd(simplexml_load_string($municipioXML->DelMunResult)->delmun);
-            $municipios=[];
-            foreach (simplexml_load_string($municipioXML->DelMunResult)->delmun as $value) {
-                $object = json_decode(json_encode($value),true);
-                array_push($municipios, ['id'=>$object['@attributes']['id'],'descripcion'=>$object[0]]);
-            }
+            $municipios=$this->convertXMLtoArray(simplexml_load_string($municipioXML->DelMunResult)->delmun);
             if (isset($municipios)) {
                 return response()->json(['municipios'=>$municipios],201);
             }
@@ -175,11 +162,7 @@ class AnaController extends Controller
         try{
             $client = new SoapClient($this->url,$this->params);
             $estadosXML = $client->EDOS(["Negocio"=>"1195","Usuario"=>"14275","Clave"=>"kdEDyC9F"]);
-            $estados=[];
-            foreach (simplexml_load_string($estadosXML->EDOSResult)->estado as $key=>$value) {
-                $object = json_decode(json_encode($value),true);
-                array_push($estados, ['id'=>$object['@attributes']['id'],'descripcion'=>$object[0]]);
-            }
+            $estados=$this->convertXMLtoArray(simplexml_load_string($estadosXML->EDOSResult)->estado);
             if (isset($estados)) {
                 return response()->json(['estados'=>$estados],201);
             }
@@ -195,11 +178,8 @@ class AnaController extends Controller
         try{
             $client = new SoapClient($this->url,$this->params);
             $formaPagoXML = $client->FormaPago(["Negocio"=>"1195","Usuario"=>"14275","Clave"=>"kdEDyC9F"]);
-            // dd($formaPagoXML);
-            $formaPagoResp = json_decode(json_encode(simplexml_load_string($formaPagoXML->FormaPagoResult)),true);
-            // dd($formaPagoResp);
-            if (isset($formaPagoResp['formapago'])) {
-                $formapagos = $formaPagoResp['formapago'];
+            $formapagos=$this->convertXMLtoArray(simplexml_load_string($formaPagoXML->FormaPagoResult)->formapago);
+            if (isset($formapagos)) {
                 return response()->json(['formapagos'=>$formapagos],201);
             }
             else{
@@ -214,11 +194,8 @@ class AnaController extends Controller
         try{
             $client = new SoapClient($this->url,$this->params);
             $giroXML = $client->GiroEmpresa(["Negocio"=>"1195","Usuario"=>"14275","Clave"=>"kdEDyC9F"]);
-            // dd($giroXML);
-            $giroResp = json_decode(json_encode(simplexml_load_string($giroXML->GiroEmpresaResult)),true);
-            // dd($giroResp);
-            if (isset($giroResp['giroempresa'])) {
-                $giros = $giroResp['giroempresa'];
+            $giros = $this->convertXMLtoArray(simplexml_load_string($giroXML->GiroEmpresaResult)->giroempresa);
+            if (isset($giros)) {
                 return response()->json(['giros'=>$giros],201);
             }
             else{
@@ -233,11 +210,8 @@ class AnaController extends Controller
         try{
             $client = new SoapClient($this->url,$this->params);
             $identificacionesXML = $client->Identificacion(["Negocio"=>"1195","Usuario"=>"14275","Clave"=>"kdEDyC9F"]);
-            // dd($identificacionesXML);
-            $identificacionesResp = json_decode(json_encode(simplexml_load_string($identificacionesXML->IdentificacionResult)),true);
-            // dd($identificacionesResp);
-            if (isset($identificacionesResp['identificiacion'])) {
-                $identificaciones = $identificacionesResp['identificiacion'];
+            $identificaciones=$this->convertXMLtoArray(simplexml_load_string($identificacionesXML->IdentificacionResult)->identificiacion);
+            if (isset($identificaciones)) {
                 return response()->json(['identificaciones'=>$identificaciones],201);
             }
             else{
@@ -252,11 +226,8 @@ class AnaController extends Controller
         try{
             $client = new SoapClient($this->url,$this->params);
             $marcasXML = $client->Marca(["Negocio"=>"1195","Modelo"=>$modelo,"Categoria"=>100,"Usuario"=>"14275","Clave"=>"kdEDyC9F"]);
-            // dd($marcasXML);
-            $marcasResp = json_decode(json_encode(simplexml_load_string($marcasXML->MarcaResult)),true);
-            // dd($marcasResp);
-            if (isset($marcasResp['marca'])) {
-                $marcas = $marcasResp['marca'];
+            $marcas=$this->convertXMLtoArray(simplexml_load_string($marcasXML->MarcaResult)->marca);
+            if (isset($marcas)) {
                 return response()->json(['marcas'=>$marcas],201);
             }
             else{
@@ -271,9 +242,7 @@ class AnaController extends Controller
         try{
             $client = new SoapClient($this->url,$this->params);
             $modelosXML = $client->Modelo(["Negocio"=>"1195","Usuario"=>"14275","Clave"=>"kdEDyC9F"]);
-            // dd($modelosXML);
             $modelosResp = json_decode(json_encode(simplexml_load_string($modelosXML->ModeloResult)),true);
-            // dd($modelosResp);
             if (isset($modelosResp['modelo'])) {
                 $modelos = $modelosResp['modelo'];
                 return response()->json(['modelos'=>$modelos],201);
@@ -285,16 +254,15 @@ class AnaController extends Controller
             dd($fault);
         }
     }
+   
     public function nacionalidades()
     {
         try{
             $client = new SoapClient($this->url,$this->params);
             $nacionalidadesXML = $client->Nacionalidad(["Negocio"=>"1195","Usuario"=>"14275","Clave"=>"kdEDyC9F"]);
-            // dd($nacionalidadesXML);
-            $nacionalidadesResp = json_decode(json_encode(simplexml_load_string($nacionalidadesXML->NacionalidadResult)),true);
-            // dd($nacionalidadesResp);
-            if (isset($nacionalidadesResp['nacionalidad'])) {
-                $nacionalidades = $nacionalidadesResp['nacionalidad'];
+            $xml = simplexml_load_string($nacionalidadesXML->NacionalidadResult)->nacionalidad;
+            $nacionalidades =  $this->convertXMLtoArray($xml);
+            if (isset($nacionalidades)) {
                 return response()->json(['nacionalidades'=>$nacionalidades],201);
             }
             else{
@@ -309,11 +277,10 @@ class AnaController extends Controller
         try{
             $client = new SoapClient($this->url,$this->params);
             $ocupacionesXML = $client->Ocupacion(["Negocio"=>"1195","Usuario"=>"14275","Clave"=>"kdEDyC9F"]);
-            // dd($ocupacionesXML);
-            $ocupacionesResp = json_decode(json_encode(simplexml_load_string($ocupacionesXML->OcupacionResult)),true);
+            $xml=simplexml_load_string($ocupacionesXML->OcupacionResult)->ocupacion;
+            $ocupaciones = $this->convertXMLtoArray($xml);
             // dd($ocupacionesResp);
-            if (isset($ocupacionesResp['ocupacion'])) {
-                $ocupaciones = $ocupacionesResp['ocupacion'];
+            if (isset($ocupaciones)) {
                 return response()->json(['ocupaciones'=>$ocupaciones],201);
             }
             else{
@@ -328,11 +295,13 @@ class AnaController extends Controller
         try{
             $client = new SoapClient($this->url,$this->params);
             $submarcasXML = $client->SubMarca(["Negocio"=>"1195","Marca"=>$marca,"Modelo"=>$modelo,"Categoria"=>100,"Usuario"=>"14275","Clave"=>"kdEDyC9F"]);
-            dd($submarcasXML);
-            $submarcasResp = json_decode(json_encode(simplexml_load_string($submarcasXML->SubMarcaResult)),true);
-            // dd($submarcasResp);
-            if (isset($submarcasResp['submarca'])) {
-                $submarcas = $submarcasResp['submarca'];
+            // dd(simplexml_load_string($submarcasXML->SubMarcaResult)->submarca);
+            $submarcas=[];
+            foreach (simplexml_load_string($submarcasXML->SubMarcaResult)->submarca as $value) {
+                $object=json_decode(json_encode($value),true);
+                array_push($submarcas,['id'=>$object['@attributes']['clave'],'descripcion'=>$object[0]]);
+            }
+            if (isset($submarcas)) {
                 return response()->json(['submarcas'=>$submarcas],201);
             }
             else{
@@ -367,15 +336,17 @@ class AnaController extends Controller
         try{
             $client = new SoapClient($this->url,$this->params);
             $vehiculosXML = $client->Vehiculo(["Negocio"=>"1195","Marca"=>$marca,"Submarca"=>$submarca,"Modelo"=>$modelo,"Usuario"=>"14275","Clave"=>"kdEDyC9F"]);
-            // dd($vehiculosXML);
-            $vehiculosResp = json_decode(json_encode(simplexml_load_string($vehiculosXML->VehiculoResult)),true);
-            // dd($vehiculosResp);
-            if (isset($vehiculosResp['vehiculo'])) {
-                $vehiculos = $vehiculosResp['vehiculo'];
+            // dd(simplexml_load_string($vehiculosXML->VehiculoResult)->vehiculo);
+            $vehiculos=[];
+            foreach (simplexml_load_string($vehiculosXML->VehiculoResult)->vehiculo as $value) {
+                $object = json_decode(json_encode($value),true);
+                array_push($vehiculos,['clave'=>$object['@attributes']['clave'],'descripcion'=>$object[0]]);
+            }
+            if (isset($vehiculos)) {
                 return response()->json(['vehiculos'=>$vehiculos],201);
             }
             else{
-                return response()->json(['error'=>"Sub-Marcas no encontradas",404]);
+                return response()->json(['error'=>"Descripciones no encontradas",404]);
             }
         }catch(SoapFault $fault){
             dd($fault);
@@ -386,15 +357,17 @@ class AnaController extends Controller
         try{
             $client = new SoapClient($this->url,$this->params);
             $catalogosXML = $client->CatVeh(["Negocio"=>"1195","ModeloMin"=>$modeloMin,"ModeloMax"=>$modeloMax,"Categ"=>100,"Usuario"=>"14275","Clave"=>"kdEDyC9F"]);
-            // dd($catalogosXML);
-            $catalogosResp = json_decode(json_encode(simplexml_load_string($catalogosXML->CatVehResult)),true);
-            dd($catalogosResp);
-            if (isset($catalogosResp['vehiculo'])) {
-                $catalogos = $catalogosResp['vehiculo'];
+            // dd(simplexml_load_string($catalogosXML->CatVehResult)->vehiculo);
+            $catalogos=[];
+            foreach (simplexml_load_string($catalogosXML->CatVehResult)->vehiculo as $value) {
+                $object=json_decode(json_encode($value),true);
+                array_push($catalogos,['categoria'=>$object['@attributes']['categoria'],'modelo'=>$object['@attributes']['modelo'],'clave_armadora'=>$object['@attributes']['cvearmadora'],'armadora'=>$object['@attributes']['armadora'],'cvesubmarca'=>$object['@attributes']['cvesubmarca'],'submarca'=>$object['@attributes']['submarca'],'clave_amis'=>$object['@attributes']['cveamis'],'descripcion'=>$object[0]]);
+            }
+            if (isset($catalogos)) {
                 return response()->json(['catalogos'=>$catalogos],201);
             }
             else{
-                return response()->json(['error'=>"Sub-Marcas no encontradas",404]);
+                return response()->json(['error'=>"Descripciones no encontradas",404]);
             }
         }catch(SoapFault $fault){
             dd($fault);
